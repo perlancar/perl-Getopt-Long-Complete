8367664af0a958b65c8380bb42a7aa6055010fc7
perlancar (@pc-office) <perlancar@gmail.com>
2015-12-30 14:27:43 +0700

    [ux] Use Complete::Bash's new truncate_current_word option for convenience, like in Perinci::Sub::Complete

--- lib/Getopt/Long/Complete.pm
+++ lib/Getopt/Long/Complete.pm
@@ -48,7 +48,7 @@ sub GetOptionsWithCompletion {
         my ($words, $cword);
         if ($ENV{COMP_LINE}) {
             require Complete::Bash;
-            ($words,$cword) = @{ Complete::Bash::parse_cmdline() };
+            ($words,$cword) = @{ Complete::Bash::parse_cmdline(undef, undef, {truncate_current_word=>1}) };
         } elsif ($ENV{COMMAND_LINE}) {
             require Complete::Tcsh;
             $shell = 'tcsh';
